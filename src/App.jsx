d76bcf207bc14604edc02df0fce322d8b60ec315
Berie <bberaki2015@gmail.com>
2024-03-01 18:13:38 -0800

    testing netlify

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div><h1>My react</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
