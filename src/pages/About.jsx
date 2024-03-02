5fcb7c329765e272b725c9ca26cc458c00c17924
Berie <bberaki2015@gmail.com>
2024-03-02 12:36:44 -0800

    updated about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,16 +5,20 @@ export default function About() {
       <div className="mt-3">
       <i className="fa fa-user fa-5x"></i>
       </div>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      <ul>
 
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae modi voluptatem ad architecto obcaecati labore perferendis, repellat amet a illo nesciunt minus quos in, eligendi animi eos cum? Cupiditate quasi, a, voluptatibus doloremque veniam cum sequi reiciendis corporis esse nobis fuga ab, maxime repudiandae eveniet labore. Qui doloribus omnis eius harum quo quaerat officiis nihil ipsum exercitationem necessitatibus perspiciatis hic aliquam nulla ipsam minus corrupti nam velit magnam itaque, temporibus alias. Tempora enim dolore fugiat. Itaque numquam sapiente aliquam tenetur ea suscipit fugiat delectus at reprehenderit accusantium, laboriosam autem officiis doloremque a repellendus explicabo beatae ducimus natus expedita. Iste rem corporis quos enim iusto ducimus facere voluptatibus nesciunt quasi error laborum, ut vitae possimus repudiandae sapiente! Commodi dicta maiores soluta velit optio necessitatibus nisi culpa repellendus dolorem saepe, voluptate quos molestias? Cum accusamus, optio soluta molestiae ad dolorem sit laudantium natus qui sapiente atque aliquam ullam aut quia quo dolorum porro aperiam cupiditate earum illum reprehenderit omnis iusto! Voluptatibus totam.
-      </p>
+    <li>Result-driven Software Engineer with more than 10 years track record of high performance in modular and object-oriented programming.
+    </li>
+    <li>
+    Extensive experience developing Web-Based Applications, Frontend & Backend Design & Development, Implementation & maintenance, Unit Testing, Data mining and Analysis.
+    </li>
+    <li>
+    Expertise in Web development using Python, Django (or other MVC frameworks), JavaScript/jQuery, CSS, front-end JS Frameworks (ReactJS) and HTML.
+    </li>
+    <li>
+    Very conscientious and reliable with strong working knowledge of algorithms, data structures, troubleshooting and analytical and problem-solving skills.
+    </li>
+     </ul>
     </section>
   );
 }
